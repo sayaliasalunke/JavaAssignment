8c33d9887b984381cc2a5abc046d1cc22f9a60ed
sayaliasalunke <salunkesayali2003@gamil.com>
2026-02-04 23:56:36 +0530

    Added assignment4

--- eclipse-workspace/Assignment/src/assignment1/CheckNumber2.java
+++ eclipse-workspace/Assignment/src/assignment1/CheckNumber2.java
@@ -4,7 +4,7 @@ package assignment1;
 
 import java.util.Scanner;
 
-public class CheckNumber {
+public class CheckNumber2 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
