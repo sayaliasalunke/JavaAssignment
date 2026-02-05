7b505856cf27ec7863a26d3851505494fdf2b103
sayaliasalunke <salunkesayali2003@gamil.com>
2026-02-05 23:45:40 +0530

    Added  Assignment5 folder

---
+++ eclipse-workspace/Assignment/src/assignment5/Palindrome.java
@@ -0,0 +1,26 @@
+package assignment5;
+
+
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String rev = "";
+
+        for(int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        if(str.equals(rev)) {
+            System.out.println("It is Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
---
+++ eclipse-workspace/Assignment/src/assignment5/WordCount.java
@@ -0,0 +1,16 @@
+package assignment5;
+
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String str = sc.nextLine();
+
+        String words[] = str.trim().split("\\s+");
+
+        System.out.println("Number of words: " + words.length);
+    }
+}
